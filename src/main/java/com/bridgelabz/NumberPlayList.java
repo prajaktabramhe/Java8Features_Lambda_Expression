8770a9a5163001e2ee22297900a96cbbdf303282
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-03-23 11:32:54 +0530

    [Prajakta]: Refator code. given proper indentation

--- src/main/java/com/bridgelabz/NumberPlayList.java
+++ src/main/java/com/bridgelabz/NumberPlayList.java
@@ -7,62 +7,56 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
-public class NumberPlayList
-{
-    public static void main(String[] args)
-    {
+public class NumberPlayList {
+    public static void main(String[] args) {
         // creating simple collection
         List<Integer> myNumberList = new ArrayList<Integer>();
         for (int i = 0; i < 5; i++) myNumberList.add(1);
 
 
-    //Method1: Traversing using Iterator
-    Iterator<Integer> it = myNumberList.iterator();
-    while(it.hasNext())
-    {
-        Integer i = it.next();
-        System.out.println("Iterator Value::" + i);
-    }
-    //Method 2: Traversing with Consumer integer implementation
-    class MyConsumer implements Consumer<Integer>
-    {
-        public void  accept(Integer t)
-        {
-            System.out.println(("Mth2: Consumer impl Value::" + t));
+        //Method1: Traversing using Iterator
+        Iterator<Integer> it = myNumberList.iterator();
+        while (it.hasNext()) {
+            Integer i = it.next();
+            System.out.println("Iterator Value::" + i);
         }
-    }
-    MyConsumer action = new MyConsumer();
+        //Method 2: Traversing with Consumer integer implementation
+        class MyConsumer implements Consumer<Integer> {
+            public void accept(Integer t) {
+                System.out.println(("Mth2: Consumer impl Value::" + t));
+            }
+        }
+        MyConsumer action = new MyConsumer();
         myNumberList.forEach(action);
 
-    //Method3: Traversing with Anonymous Consumer interface implementations
-        myNumberList.forEach(new Consumer<Integer>()
-    {
-        public void accept(Integer t)
-        {
-            System.out.println("Mth3: forEach anonymous class Value::"+t);
-        }
-    });
+        //Method3: Traversing with Anonymous Consumer interface implementations
+        myNumberList.forEach(new Consumer<Integer>() {
+            public void accept(Integer t) {
+                System.out.println("Mth3: forEach anonymous class Value::" + t);
+            }
+        });
 
-    //Method 4:Explicit Lambda Function
-    Consumer<Integer> myListAction = n-> {
-        System.out.println(("Mth4: forEach Lambda impl Value::" + n));
-    };
+        //Method 4:Explicit Lambda Function
+        Consumer<Integer> myListAction = n -> {
+            System.out.println(("Mth4: forEach Lambda impl Value::" + n));
+        };
         myNumberList.forEach(myListAction);
 
-    //Method 5: Implicit Lambda Function
-        myNumberList.forEach( n->{
-        System.out.println(("Mth5: forEach Lambda impl Value::" + n));
-    });
-
-    //Method 6: Implicit Lambda function to print double value
-        Function<Integer,Double> toDoubleFunction = Integer::doubleValue;
+        //Method 5: Implicit Lambda Function
         myNumberList.forEach(n -> {
-        System.out.println(("Mth6: forEach Lambda impl Value::" + toDoubleFunction.apply(n)));
-    });
-    //Method 6: Implicit Lambda function to print double value
+            System.out.println(("Mth5: forEach Lambda impl Value::" + n));
+        });
+
+        //Method 6: Implicit Lambda function to print double value
+        Function<Integer, Double> toDoubleFunction = Integer::doubleValue;
+        myNumberList.forEach(n -> {
+            System.out.println(("Mth6: forEach Lambda impl Value::" + toDoubleFunction.apply(n)));
+        });
+
+        //Method 7: Implicit Lambda function to print double value
         Predicate<Integer> isEvenFunction = n -> n > 0 && n%2 == 0;
         myNumberList.forEach(n -> {
-        System.out.println(("Mth7: forEach Lambda impl Value::" + isEvenFunction.test(n)));
-            });
+            System.out.println(("Mth7: forEach Lambda impl Value::" + isEvenFunction.test(n)));
+        });
     }
 }
