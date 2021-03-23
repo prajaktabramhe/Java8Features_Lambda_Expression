80eca38a85314b380d85667ea027ff7726e9fdb7
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-03-23 22:57:56 +0530

    [Prajakta]: Refactor code. Add file

--- src/main/java/com/bridgelabz/NumberPlayList.java
+++ src/main/java/com/bridgelabz/NumberPlayList.java
@@ -12,51 +12,50 @@ public class NumberPlayList
     {
         // creating simple collection
         List<Integer> myNumberList = new ArrayList<Integer>();
-        for (int i = 0; i < 5; i++) myNumberList.add(1);
+        for (int i = 0; i < 5; i++) myNumberList.add(i);
 
-
-    //Method1: Traversing using Iterator
-    Iterator<Integer> it = myNumberList.iterator();
-    while(it.hasNext())
-    {
-        Integer i = it.next();
-        System.out.println("Iterator Value::" + i);
-    }
-    //Method 2: Traversing with Consumer integer implementation
-    class MyConsumer implements Consumer<Integer>
-    {
-        public void  accept(Integer t)
+       //Method1: Traversing using Iterator
+        Iterator<Integer> it = myNumberList.iterator();
+        while(it.hasNext())
         {
-            System.out.println(("Mth2: Consumer impl Value::" + t));
+            Integer i = it.next();
+            System.out.println("Iterator Value::" + i);
         }
-    }
-    MyConsumer action = new MyConsumer();
-        myNumberList.forEach(action);
-
-    //Method3: Traversing with Anonymous Consumer interface implementations
-        myNumberList.forEach(new Consumer<Integer>()
-    {
-        public void accept(Integer t)
+        //Method 2: Traversing with Consumer integer implementation
+        class MyConsumer implements Consumer<Integer>
         {
-            System.out.println("Mth3: forEach anonymous class Value::"+t);
+            public void  accept(Integer t)
+            {
+                System.out.println(("Mth2: Consumer impl Value::" + t));
+            }
         }
-    });
+        MyConsumer action = new MyConsumer();
+            myNumberList.forEach(action);
 
-    //Method 4:Explicit Lambda Function
-    Consumer<Integer> myListAction = n-> {
-        System.out.println(("Mth4: forEach Lambda impl Value::" + n));
-    };
-        myNumberList.forEach(myListAction);
+        //Method3: Traversing with Anonymous Consumer interface implementations
+            myNumberList.forEach(new Consumer<Integer>()
+        {
+            public void accept(Integer t)
+            {
+                System.out.println("Mth3: forEach anonymous class Value::"+t);
+            }
+        });
 
-    //Method 5: Implicit Lambda Function
-        myNumberList.forEach( n->{
-        System.out.println(("Mth5: forEach Lambda impl Value::" + n));
-    });
+        //Method 4:Explicit Lambda Function
+        Consumer<Integer> myListAction = n-> {
+            System.out.println(("Mth4: forEach Lambda impl Value::" + n));
+        };
+            myNumberList.forEach(myListAction);
 
-    //Method 6: Implicit Lambda function to print double value
-        Function<Integer,Double> toDoubleFunction = Integer::doubleValue;
-        myNumberList.forEach(n -> {
-            System.out.println(("Mth5: forEach Lambda impl Value::" + toDoubleFunction.apply(n)));
-    });
+        //Method 5: Implicit Lambda Function
+            myNumberList.forEach( n->{
+            System.out.println(("Mth5: forEach Lambda impl Value::" + n));
+        });
+
+        //Method 6: Implicit Lambda function to print double value
+            Function<Integer,Double> toDoubleFunction = Integer::doubleValue;
+            myNumberList.forEach(n -> {
+                System.out.println(("Mth5: forEach Lambda impl Value::" + toDoubleFunction.apply(n)));
+        });
     }
 }
